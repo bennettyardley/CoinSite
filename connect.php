0156d9591a1fc7cca05d6b420cc6351a6e86b5b0
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-07-12 14:21:00 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,5 +4,5 @@ DEFINE ('DB_PSWD', 'XXXXXXXXX');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'XXXXXXXXX');
 
-$dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
+$ = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
 ?>
