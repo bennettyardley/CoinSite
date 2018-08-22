beb6e7fca7a7f1e6ee35958bd2cfafa2130c1a4e
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-08-21 21:19:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>Home</title>
-<link rel="" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="script.js"></script>  
